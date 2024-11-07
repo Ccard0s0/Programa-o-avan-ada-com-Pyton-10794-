e9b9ab7e0f3fd4f3503219bd8fc7bb910a5e6383
Carolina Cardoso <146833352+Ccard0s0@users.noreply.github.com>
2024-11-07 15:16:47 +0000

    bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -13,6 +14,10 @@ def modulos():
 @app.route('/seguranca')
 def seguranca():
     return render_template('seguranca.html')
+
+@app.route('/bootstrap')
+def bootstrap():
+    return render_template('bootstrap.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
